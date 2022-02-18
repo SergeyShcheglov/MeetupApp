10c91720d9aab79f098ef88113a32e2ca0e7254a
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-18 14:48:52 +0600

    Creat MeetupApp app

---
+++ MeetupApp/MeetupAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetupAppApp.swift
+//  MeetupApp
+//
+//  Created by Sergey Shcheglov on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MeetupAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
