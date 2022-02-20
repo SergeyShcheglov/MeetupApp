a36bdf4378708c8cb5fc61a141725784180cd33e
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-20 14:02:33 +0600

    Add some fixes

--- MeetupApp/ContentView.swift
+++ MeetupApp/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var contentViewModel = ViewModel()
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            NavigationLink {
+                DetailView()
+            } label: {
+                List(contentViewModel.person, id: \.id) { person in
+                    HStack {
+                        Image(person.photo ?? "person.circle")
+                        
+                        VStack(alignment: .leading) {
+                            Text("\(person.firstName) \(person.lastName)")
+                            Text("some descritpion")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("MeetupApp")
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {
+                        contentViewModel.addButtonTapped = true},
+                           label: {
+                        Image(systemName: "plus")
+                    })
+                }
+            }
+            .sheet(isPresented: $contentViewModel.addButtonTapped) {
+                AddContactView()
+            }
+        }
     }
 }
 
