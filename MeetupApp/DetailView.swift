604baa5d210235efeca850e9f3d24a80e0bbbf43
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-22 17:22:13 +0600

    Make this things work

--- MeetupApp/DetailView.swift
+++ MeetupApp/DetailView.swift
@@ -8,20 +8,30 @@
 import SwiftUI
 
 struct DetailView: View {
-    @ObservedObject var contentViewModel = ViewModel()
+    var contact: ContactsModel
+    
+    let savePaths = FileManager.documentDirectory
     
     var body: some View {
         VStack {
-            Image(systemName: "heart")
-            Text("Full name")
+            getPhotoFrom(uuid: contact.wrappedPhotoId)
+                .resizable()
+                .frame(maxHeight: UIScreen.main.bounds.height * 0.6)
+            Text("\(contact.firstName ?? "") \(contact.lastName ?? "")")
+            Spacer()
         }
     }
-    
+    func getPhotoFrom(uuid: UUID) -> Image {
+        let uuidString = uuid.uuidString
+        guard let data = try? Data(contentsOf: savePaths.appendingPathComponent(uuidString)) else { return Image(systemName: "person.crop.circle.badge.questionmark") }
+        guard let uiImage = UIImage(data: data, scale: 1.0) else { return Image(systemName: "person.crop.circle") }
+        return Image(uiImage: uiImage)
+    }
     
 }
 
-struct DetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailView()
-    }
-}
+//struct DetailView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DetailView()
+//    }
+//}
