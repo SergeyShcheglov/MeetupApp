53c523a6b34c1cde0531a4345405426342046689
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-25 16:22:05 +0600

    Add showPhotoPicker method

--- MeetupApp/ViewModel.swift
+++ MeetupApp/ViewModel.swift
@@ -11,7 +11,21 @@ class ViewModel: ObservableObject {
     @Published var person = [PersonContacts]()
     
     @Published var addButtonTapped = false
+    @Published var source: Picker.Source = .library
     @Published var showingImagePicker = false
+    @Published var showCameraAlert = false
+    @Published var cameraError: Picker.CameraErrorType?
     
+    func showPhotoPicker() {
+        do {
+            if source == .camera {
+                try Picker.checkPermissions()
+            }
+            showingImagePicker = true
+        } catch {
+            showCameraAlert = true
+            cameraError = Picker.CameraErrorType(error: error as! Picker.PickerError)
+        }
+    }
 }
 
