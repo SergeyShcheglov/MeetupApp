9d27a44c26d32832d82ca1e768c9d74c027298b7
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-27 16:55:46 +0600

    Add Class to fetch user's location

---
+++ MeetupApp/Model/Location.swift
@@ -0,0 +1,28 @@
+//
+//  Location.swift
+//  MeetupApp
+//
+//  Created by Sergey Shcheglov on 27.02.2022.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+    }
+}
